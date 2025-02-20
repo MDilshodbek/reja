30594d588824e9d7cab7bc587545584e38ee8a1b
MDilshodbek <mdbek55@gmail.com>
2025-02-20 19:00:31 +0900

    BRR: building express web server

---
+++ server.js
@@ -0,0 +1,41 @@
+console.log("Web serverni boshlash");
+
+const express = require("express");
+const app = express();
+const http = require(`http`)
+
+// Web server qurishda 4ta bosqich bor
+
+// step 1: Kirish code
+
+// to make the public folder available to see
+app.use(express.static("public")); 
+// json formatdagi datani object holatga ozgartirib beradi
+app.use(express.json());
+// formga kiritilgan ma'lumotlar serverga post qilinganda express qabul qilib oladi
+app.use(express.urlencoded({extended: true}));
+
+// Step 2 - session code
+
+// Step 3 - Views code/ backend site server rendering
+// npm i ejs
+app.set("views", "views"); - /* mentioning the folder */
+app.set("view engine", "ejs"); /* mentioning that the view engine is ejs */
+
+// Step 4 - Routing code
+app.get("/hello", function(req, res){
+res.end("<h1>Hello World by Oscar</h1>");
+});
+
+app.get("/gift", function(req, res){
+    res.end(`<h1>Siz sovg'alar packagedasiz</h1>`);
+    
+})
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function () {
+console.log(`The server is running successfully on port: ${PORT}`);
+});
+
+// Loyihalar master va develop branchlaridan iborat
