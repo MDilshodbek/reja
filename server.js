76209578cfdfcf097291d1eeefb0a948e16ca76b
MDilshodbek <mdbek55@gmail.com>
2025-02-22 16:43:09 +0900

    Added portfolio author.ejs file

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 console.log("Web serverni boshlash");
 
 const express = require("express");
+const res = require("express/lib/response")
 const app = express();
 const http = require(`http`)
+const fs = require("fs")
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err){
+        console.log("ERROR:", err);        
+    }else {
+        user = JSON.parse(data)
+    }
+})
 
 // Web server qurishda 4ta bosqich bor
 
@@ -39,6 +50,10 @@ app.post("/create-item", (req, res) => {
 app.get("/", function(req, res){
     res.render(`harid`)
     
+})
+
+app.get('/author', (req, res)=> {
+    res.render("author", {user: user} )
 })
 
 const server = http.createServer(app);
