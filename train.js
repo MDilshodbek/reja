44e7e7465ed54f264eb5df0bf47d552134d93d78
MDilshodbek <mdbek55@gmail.com>
2025-02-24 23:53:15 +0900

    Nodejs event loop & callback funcs

--- train.js
+++ train.js
@@ -1,18 +1,52 @@
 // console.log(`Train task ishga tushdi`);
 
-const countLetter = (a, b) => {
+// const countLetter = (a, b) => {
 
-    let count = 0
+//     let count = 0
     
-    for (let i = 0; i < b.length; i++) {
-       if (b[i] == a) {
-        count++
-       }
-    }
+//     for (let i = 0; i < b.length; i++) {
+//        if (b[i] == a) {
+//         count++
+//        }
+//     }
     
-     return count;
-    }
+//      return count;
+//     }
 
-    console.log(countLetter("e", "engineer"));
+//     console.log(countLetter("e", "engineer"));
+
+// console.log("Callback functions");
+
+// const list = [
+//    "0-20 Loremskfskerjbjkerlkj",
+//    "20-30 Loremmdksfvnkevjenvsjknkfjnsk",
+//    "30-40 Loremmakefbewkkfhbewj",
+//    "40-50 Loremakdbhcjwckah",
+//    "50-60 Loremajcbahwbcj",
+//    "60 LoremKBAHWBEFBBBBBNNNNNNNNNNNN"
+// ]
+
+// function maslahatBering(a, callback){
+//    if (typeof a !== "number") callback("Insert number", null);
+//    else if(a <= 20) callback(null, list[0]);
+//    else if(a > 20 && a <= 30) callback(null, list[1]);
+//    else if(a > 20 && a <= 30) callback(null, list[2]);
+//    else if(a > 30 && a <= 40) callback(null, list[3]);
+//    else if(a > 40 && a <= 50) callback(null, list[4]);
+//    else {
+//       setTimeout(function(){
+//          callback(null, list[5])
+//       }, 5000)
+//    }
+// }
+
+// console.log("passed here");
+
+// maslahatBering(65, (err, data) => {
+//    if(err) console.log('ERROR:', err);
+//    console.log('javob:', data);
+// })
+
+// console.log("passed here2");
     
     
