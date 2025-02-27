a27f9dc202230cde436b11d2d9c42c06cbd5a2fd
MDilshodbek <mdbek55@gmail.com>
2025-02-27 19:13:59 +0900

    feat: mongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,63 @@
+console.log("Web serverni boshlash");
+const express = require("express");
+const app = express();
+
+const res = require("express/lib/response")
+const fs = require("fs")
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err){
+        console.log("ERROR:", err);        
+    }else {
+        user = JSON.parse(data)
+    }
+})
+// MongoDB connect
+const db = require("./server").db()
+ 
+
+// Web server qurishda 4ta bosqich bor
+
+// step 1: Kirish code
+
+// to make the public folder available to see
+app.use(express.static("public")); 
+// json formatdagi datani object holatga ozgartirib beradi
+app.use(express.json());
+// formga kiritilgan ma'lumotlar serverga post qilinganda express qabul qilib oladi
+app.use(express.urlencoded({extended: true}));
+
+// Step 2 - session code
+
+// Step 3 - Views code/ backend site server rendering
+// npm i ejs
+app.set("views", "views"); - /* mentioning the folder */
+app.set("view engine", "ejs"); /* mentioning that the view engine is ejs */
+
+// Step 4 - Routing code
+app.get("/hello", function(req, res){
+res.end("<h1>Hello World by Oscar</h1>");
+});
+
+app.get("/gift", function(req, res){
+    res.end(`<h1>Siz sovg'alar packagedasiz</h1>`);
+})
+
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"})
+})
+
+app.get("/", function(req, res){
+    res.render(`reja`)
+    
+})
+
+app.get('/author', (req, res)=> {
+    res.render("author", {user: user} )
+})
+
+module.exports = app;
+
+// Loyihalar master va develop branchlaridan iborat
--- server.js
+++ server.js
@@ -1,65 +1,23 @@
-console.log("Web serverni boshlash");
-
-const express = require("express");
-const app = express();
 const http = require(`http`)
-const res = require("express/lib/response")
-const fs = require("fs")
+const mongodb = require("mongodb")
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err){
-        console.log("ERROR:", err);        
-    }else {
-        user = JSON.parse(data)
+const connectionString = "mongodb+srv://mdbek55:woosong2024!@mdbek.fyev9.mongodb.net/Reja?retryWrites=true&w=majority&appName=MDbek"
+let db;
+
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else{
+        console.log("MongoDB connection succeed");
+        console.log(client);
+        
+        module.exports = client;
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function () {
+        console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+        });
+  
+        
     }
 })
-
-// Web server qurishda 4ta bosqich bor
-
-// step 1: Kirish code
-
-// to make the public folder available to see
-app.use(express.static("public")); 
-// json formatdagi datani object holatga ozgartirib beradi
-app.use(express.json());
-// formga kiritilgan ma'lumotlar serverga post qilinganda express qabul qilib oladi
-app.use(express.urlencoded({extended: true}));
-
-// Step 2 - session code
-
-// Step 3 - Views code/ backend site server rendering
-// npm i ejs
-app.set("views", "views"); - /* mentioning the folder */
-app.set("view engine", "ejs"); /* mentioning that the view engine is ejs */
-
-// Step 4 - Routing code
-app.get("/hello", function(req, res){
-res.end("<h1>Hello World by Oscar</h1>");
-});
-
-app.get("/gift", function(req, res){
-    res.end(`<h1>Siz sovg'alar packagedasiz</h1>`);
-})
-
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"})
-})
-
-app.get("/", function(req, res){
-    res.render(`reja`)
-    
-})
-
-app.get('/author', (req, res)=> {
-    res.render("author", {user: user} )
-})
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-});
-
-// Loyihalar master va develop branchlaridan iborat
